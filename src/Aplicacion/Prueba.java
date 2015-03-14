25fd4e46ffc8fa4dcc6f5659ca8489a995c5195c
Carlos Mario Carmona <cm.carmona18@outlook.com>
2015-03-13 19:44:47 -0700

    Otra Prueba

--- src/Aplicacion/Prueba.java
+++ src/Aplicacion/Prueba.java
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Aplicacion;
-
-/**
- *
- * @author CarlosMario
- */
 public class Prueba {
-    
-    
+    //Esto es una clase prueba     
 }
