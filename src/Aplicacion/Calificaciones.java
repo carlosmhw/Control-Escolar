ce8eac113abc2965dffaa2acbc505425fd015055
AraelGomez <arael19@outlok.com>
2015-04-02 18:58:18 -0700

    Pantalla Calificaciones

--- src/Aplicacion/Calificaciones.java
+++ src/Aplicacion/Calificaciones.java
@@ -2,17 +2,81 @@
 
 
 package Aplicacion;
+
+import Database.OracleBD;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.table.DefaultTableModel;
+
 public class Calificaciones extends javax.swing.JFrame {
     String usuario;
+    public String matricula, nombre;
     boolean flag_habilitar = false;
+    public DefaultTableModel modelo = new DefaultTableModel(){
+
+    @Override
+    public boolean isCellEditable(int row, int column) {
+       //all cells false
+       return false;
+    }
+};;
+    
     
     public Calificaciones(String tipoUsuario, String matriculaAL){
-        this.usuario = tipoUsuario;  
-        initComponents(); 
+        this.matricula=matriculaAL;
+        modelo.addColumn("ID");
+        modelo.addColumn("Semestre");
+        modelo.addColumn("Materia");
+        modelo.addColumn("Parcial 1");
+        modelo.addColumn("Parcial 1");
+        modelo.addColumn("Parcial 1");
+        modelo.addColumn("Promedio");
+        this.usuario = tipoUsuario; 
+        System.out.println(matriculaAL);
+        System.out.println(usuario);
+        initComponents();
         if(usuario.equals("ALUMNO")){
             ocultarCampos();
             ocultarBotones();
+            //Conexcion de solo materias que este cursando el morro
+            
+        }else{
+            habilitarBotones(flag_habilitar);
+            habilitarCampos(flag_habilitar);
+            OracleBD OracleConnection = new OracleBD();
+            try {
+                OracleConnection.conectar();
+                Connection conn = OracleConnection.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rset0 = stmt.executeQuery("select nombre, apellidoPaterno, apellidoMaterno from Alumno where matriculaAL='"+matriculaAL+"'");
+                while(rset0.next()){
+                    //this.nombre=rset0.getString(0);
+                    jLabelMatriculaYNombre.setText(matriculaAL +" "+rset0.getString(1)+rset0.getString(2)+rset0.getString(3));
+                }
+                ResultSet rset = stmt.executeQuery(
+                    "select idmateria,semestre, M.nombre, parcial1,parcial2,parcial3,promedio,status "
+                    + "from alumno\n" +
+                    "join kardex K using(matriculaAL)\n" +
+                    "join calificaciones C using(idkardex)\n" +
+                    "join materia M using(idMateria)\n" +
+                    "where matriculaAL='"+matriculaAL+"'" );  
+                while(rset.next()){
+                    Object[] fila = new Object[7];
+                           for (int i = 0; i <= 6; i++){
+                               fila[i]=rset.getObject(i+1);
+                           }
+                           modelo.addRow(fila);
+                }
+                jTableCalificaciones.setModel(modelo);
+                stmt.close();
+                OracleConnection.cerrar();
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
         }
+        
     }
     public Calificaciones() {
         initComponents(); 
@@ -27,7 +91,7 @@ public class Calificaciones extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTableCalificaciones = new javax.swing.JTable();
         btnguardar = new javax.swing.JButton();
         btncancelar = new javax.swing.JButton();
         jComboBoxCalificacion = new javax.swing.JComboBox();
@@ -39,28 +103,34 @@ public class Calificaciones extends javax.swing.JFrame {
         lblCalif = new javax.swing.JLabel();
         btnConfirmarCalif = new javax.swing.JButton();
         componenteAyuda1 = new Aplicacion.ComponenteAyuda();
+        jButton1 = new javax.swing.JButton();
+        jLabelMatriculaYNombre = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Calificaciones");
         setResizable(false);
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTableCalificaciones.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "Semestre", "Materia", "Parcial 1", "Parcial 2", "Parcial 3", "Promedio"
             }
         ));
-        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
+        jTableCalificaciones.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jTableCalificaciones.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jTable1MouseClicked(evt);
+                jTableCalificacionesMouseClicked(evt);
             }
         });
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTableCalificaciones);
+        if (jTableCalificaciones.getColumnModel().getColumnCount() > 0) {
+            jTableCalificaciones.getColumnModel().getColumn(3).setCellEditor(new javax.swing.DefaultCellEditor(jComboBoxCalificacion));
+        }
 
         btnguardar.setText("Guardar");
         btnguardar.addActionListener(new java.awt.event.ActionListener() {
@@ -95,7 +165,7 @@ public class Calificaciones extends javax.swing.JFrame {
             }
         });
 
-        jComboBoxParcial.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Parcial 1", "Parcial 2", "Parcial 3" }));
+        jComboBoxParcial.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "PARCIAL1", "PARCIAL2", "PARCIAL3" }));
 
         btnEditar.setText("Editar");
         btnEditar.addActionListener(new java.awt.event.ActionListener() {
@@ -115,13 +185,24 @@ public class Calificaciones extends javax.swing.JFrame {
             }
         });
 
+        jButton1.setText("Cerrar Sesion");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabelMatriculaYNombre.setText("Matricula y Nombre del Alumno");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 432, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 576, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelMatriculaYNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 496, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
@@ -135,9 +216,10 @@ public class Calificaciones extends javax.swing.JFrame {
                                     .addComponent(lblCalif))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jComboBoxParcial, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jComboBoxParcial, 0, 137, Short.MAX_VALUE)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addComponent(jComboBoxCalificacion, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addComponent(jComboBoxCalificacion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                         .addComponent(lblPunto, javax.swing.GroupLayout.PREFERRED_SIZE, 4, javax.swing.GroupLayout.PREFERRED_SIZE)
                                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -145,20 +227,30 @@ public class Calificaciones extends javax.swing.JFrame {
                                         .addGap(9, 9, 9)))))
                         .addContainerGap())
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(btnEditar)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnEditar)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(jButton1)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(componenteAyuda1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                         .addGroup(layout.createSequentialGroup()
-                            .addGap(44, 44, 44)
+                            .addGap(12, 12, 12)
+                            .addComponent(jLabelMatriculaYNombre)
+                            .addGap(18, 18, 18)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addGap(35, 35, 35)
+                            .addContainerGap()
+                            .addComponent(jButton1)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(btnEditar)
                             .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -185,16 +277,57 @@ public class Calificaciones extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnguardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnguardarActionPerformed
-        //Actualizar tabla y guardar en base de datos
+        String calificacion = ((String) jComboBoxCalificacion.getSelectedItem() +"."+(String) jComboBoxDecimal.getSelectedItem());
+        String parcial = (String) jComboBoxParcial.getSelectedItem();
+        String matriculaAL= this.matricula;
+        int row = jTableCalificaciones.getSelectedRow();
+        String materia = jTableCalificaciones.getValueAt(row, 0).toString();
+        System.out.println("Mat: "+materia+" Parcial: "+parcial+" Calif: "+calificacion);
+        while(modelo.getRowCount()>0)modelo.removeRow(0);
+        OracleBD OracleConnection = new OracleBD();
+        try {
+                OracleConnection.conectar();
+                Connection conn = OracleConnection.getConnection();
+                Statement stmt = conn.createStatement();
+                stmt.executeQuery("UPDATE  (select idmateria,semestre, M.nombre, parcial1,parcial2,parcial3,promedio,status from alumno " +
+                    "join kardex K using(matriculaAL) " +
+                    "join calificaciones C using(idkardex) " +
+                    "join materia M using(idMateria) " +
+                    "where matriculaAL='"+matriculaAL+"' AND "
+                    + "idMateria ='"+materia
+                    + "') set "+parcial+" = "+calificacion);
+                ResultSet rset = stmt.executeQuery(
+                    "select idmateria,semestre, M.nombre, parcial1,parcial2,parcial3,promedio,status "
+                    + "from alumno\n" +
+                    "join kardex K using(matriculaAL)\n" +
+                    "join calificaciones C using(idkardex)\n" +
+                    "join materia M using(idMateria)\n" +
+                    "where matriculaAL='"+matriculaAL+"'" );  
+                while(rset.next()){
+                    System.out.println(rset.getString(2));
+                    Object[] fila = new Object[7];
+                           for (int i = 0; i <= 6; i++){
+                               fila[i]=rset.getObject(i+1);
+                           }
+                           modelo.addRow(fila);
+                }
+                jTableCalificaciones.setModel(modelo);
+                stmt.close();
+                OracleConnection.cerrar();
+            } catch (SQLException ex) {
+                System.out.println("Error: " + ex.getMessage());
+            }
+        
+        
         
         flag_habilitar = false;
         habilitarCampos(flag_habilitar);
         habilitarBotones(flag_habilitar);
     }//GEN-LAST:event_btnguardarActionPerformed
 
-    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+    private void jTableCalificacionesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableCalificacionesMouseClicked
         btnEditar.setEnabled(true);
-    }//GEN-LAST:event_jTable1MouseClicked
+    }//GEN-LAST:event_jTableCalificacionesMouseClicked
 
     private void btnEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditarActionPerformed
         flag_habilitar = true;
@@ -230,6 +363,12 @@ public class Calificaciones extends javax.swing.JFrame {
             jComboBoxDecimal.setEnabled(true);
         }
     }//GEN-LAST:event_jComboBoxCalificacionActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.dispose();
+        Login login = new Login();
+        login.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -272,11 +411,13 @@ public class Calificaciones extends javax.swing.JFrame {
     private javax.swing.JButton btncancelar;
     private javax.swing.JButton btnguardar;
     private Aplicacion.ComponenteAyuda componenteAyuda1;
+    private javax.swing.JButton jButton1;
     private javax.swing.JComboBox jComboBoxCalificacion;
     private javax.swing.JComboBox jComboBoxDecimal;
     private javax.swing.JComboBox jComboBoxParcial;
+    private javax.swing.JLabel jLabelMatriculaYNombre;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTableCalificaciones;
     private javax.swing.JLabel lbParcial;
     private javax.swing.JLabel lblCalif;
     private javax.swing.JLabel lblPunto;
